dde09d42821a5854e55df029bae0b4f896526b6f
Bruna Lais Lindner <brunalaislindner@outlook.com>
2025-08-16 10:54:02 -0300

    Resolução do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("99900-8588", "Modelo Nokia", "1414141", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iPhone = new Iphone("98866-5544", "Modelo iPhone", "121212", 128);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
+
